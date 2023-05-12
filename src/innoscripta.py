a95ab9314a414d2639274981e6ec21d691a7bc5b
jp martins <joao@sorcero.com>
2023-05-11 22:23:03 -0300

    created class to do innoscripta logic

---
+++ src/innoscripta.py
@@ -0,0 +1,165 @@
+import os
+
+from dotenv import load_dotenv
+from langchain import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.llms import OpenAI
+from serpapi import GoogleSearch
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+SERPAPI_KEY = os.getenv("SERPAPI_KEY")
+
+
+class Innoscripta:
+    """Class of innoscript solution"""
+
+    def __init__(self, name: str, country: str, website: str = None):
+        self.name = name
+        self.country = country
+        if not website:
+            self.website = ""
+        else:
+            self.website = website
+
+    def main(self):
+        """
+        Will do the innoscripta querying
+        """
+        parsed_gpt_ouput = self.gpt_call()
+        google_query = self.google_query_formation(parsed_gpt_ouput)
+        imgs = self.google_search(google_query)
+
+        return imgs
+
+    def gpt_call(self) -> dict:
+        """
+        Will call gpt-3.5-turbo for querying informations about a company.
+
+        Args:
+            name(str): Name of the company
+            country(str): Country of the company
+            website(str): Website of the company
+
+        Results:
+            output(dict) = Parsed output of OpenAIAPI
+        """
+        llm = OpenAI(model_name="gpt-3.5-turbo", temperature=0)
+
+        prompt = self.prompt_template()
+
+        chain = LLMChain(llm=llm, prompt=prompt)
+        output = chain.run(
+            {
+                "name_of_company": self.name,
+                "country_of_company": self.country,
+                "website_of_company": self.website,
+            }
+        )
+        parsed = self.parse_output(output)
+
+        return parsed
+
+    def google_query_formation(self, products: list) -> str:
+        """
+        Will manipulate strings to create Google search query
+
+        Args:
+            name(str): Name of company
+            countr(str): Country of company
+            products(list): Products that the company offers
+
+        Return:
+            google_query(str): Google query
+        """
+        products_services_str = " + ".join(products)
+
+        return " + ".join([self.name, self.country, products_services_str])
+
+    def google_search(self, query: str) -> list:
+        """
+        Will query google for images based in the output of OpenAIAPI
+
+        Args:
+            query(str): Formated query using the output openaiAPI
+
+        Results:
+            imgs(list): List with URLs for images
+        """
+        search = GoogleSearch(
+            {
+                "q": query,
+                "engine": "google_images",
+                "location": "Austin, Texas",
+                "api_key": SERPAPI_KEY,
+            }
+        )
+        response = search.get_dict()
+        imgs = [r["original"] for r in response["images_results"][:5]]
+
+        return imgs
+
+    def prompt_template(self):
+        template = """
+        I'll give you three inputs. These inputs will be the name of the company, 
+        the country of the company, and the website company. The website of the company
+        is not mandatory.
+        You have to give me the products and services that the company offers as output.
+        you dont need to give me nothing more than the ouput.
+
+
+
+        input:
+        IKEA Deutschland GmbH & Co. KG
+        Germany
+        ikea.com
+
+        the output must be in this format, please use it:
+        "Products/services": Furniture, Home decor, Kitchen and Dining;
+        "Keywords":furniture, storage, lighting;
+        "Company Classification":5712 (Furniture Stores) – SIC, 442110 (Furniture Stores) – NAICS
+        do it yourself now.
+        input:
+        {name_of_company}
+        {country_of_company}
+        {website_of_company}
+
+        what is the output?
+        """
+
+        prompt = PromptTemplate(
+            input_variables=[
+                "name_of_company",
+                "country_of_company",
+                "website_of_company",
+            ],
+            template=template,
+        )
+
+        return prompt
+
+    def parse_output(self, output_langchain: str) -> dict:
+        """
+        Will parse the output_langchain of the Langchain query
+
+        Args:
+            output(str): The output_langchain of Langchain query.
+
+        Returns:
+            result_dict(dict): the parsed output_langchain to dict
+        """
+
+        result_dict = {}
+
+        sections = [section.strip() for section in output_langchain.split(";")]
+
+        for section in sections:
+            if section:
+                header, values_str = section.split(":")
+                header = header.strip('"')
+                values = [value.strip() for value in values_str.strip("[]").split(",")]
+
+                result_dict[header] = values
+
+        return result_dict
